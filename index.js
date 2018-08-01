a7e3e761b86144a2b84792282d9a46b900dc4627
Angel Rodriguez <angelr1076@gmail.com>
2018-08-01 16:03:35 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+setTimeout (function() {
+    document.querySelector("#modal").style = "display:block";
+}, 3000)
+
+var header = document.getElementById("modal-para");
+
+    function checkUser() {
+        var name = document.getElementById("name"),
+            email = document.getElementById("email"),
+            submit = document.getElementById("submit");
+
+        if (email.value.length > 8 && email.value.length < 30 && name.value.length > 8 && name.value.length < 25) {
+            header.innerHTML = "Congrats, you've been added to our mailing list!";
+        } else {
+            (header.innerHTML = "Please try again");
+        }
+    } 
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
